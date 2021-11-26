1629c0959db52b2a4ed46ec1c1e2a526a279da0a
Olivier Gamache <olivier@glo.quebec>
2021-11-26 13:38:56 -0500

    💃 Animated menu!

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,22 @@ import { Suspense, useState } from 'react'
 
 import Game from './components/Game'
 import Footer from './components/Footer'
-import LanguageSelector from './components/togglers/LanguageSelector'
-import ThemeToggler from './components/togglers/ThemeToggler'
-import RulesToggler from './components/togglers/RulesToggler'
+import MenuToggler from './components/togglers/MenuToggler'
 
 import './App.css'
 
 function App() {
-  const [theme, setTheme] = useState('light')
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
-  }
+  // const [theme, setTheme] = useState('light')
+  // const toggleTheme = () => {
+  //   setTheme(theme === 'light' ? 'dark' : 'light')
+  // }
 
   return (
     <Suspense fallback={<div/>}>
-      <div className="App" data-theme={theme}>
+      <div className="App" data-theme="light">
         <Game />
         <Footer />
-        <ThemeToggler theme={theme} toggleTheme={toggleTheme} />
-        <RulesToggler />
-        <LanguageSelector />
+        <MenuToggler />
       </div>
     </Suspense>
   );
---
+++ src/components/togglers/MenuToggler/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import LanguageSelector from '../LanguageSelector'
+import RulesToggler from '../RulesToggler'
+import ThemeToggler from '../ThemeToggler'
+
+import style from './style.module.css'
+
+const MenuToggler = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div className={`${style.menu} ${isOpen && style.visible}`}>
+      <div
+        className={`${style.toggle} ${isOpen && style.visible}`}
+        onClick={toggle}
+      >
+        &lt;
+      </div>
+      <div className={`${style.buttons}`}>
+        <LanguageSelector />
+        <div className={style.divider} />
+        <RulesToggler />
+        <div className={style.divider} />
+        <ThemeToggler theme="light" toggleTheme={() => undefined} />
+      </div>
+    </div>
+  )
+}
+
+export default MenuToggler
