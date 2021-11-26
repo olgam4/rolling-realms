8427e6c928a923dfc554260479913ce37a337066
Olivier Gamache <olivier@glo.quebec>
2021-11-26 10:48:56 -0500

    ☭ Total of resources gained

--- src/components/ResourceCounter/index.tsx
+++ src/components/ResourceCounter/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
 import Resource, { ResourceProps } from '../Resource'
 
 import style from './style.module.css'
@@ -14,24 +16,32 @@ const ResourceCounter = ({
   decrementScore,
   resource
 }: ResourceCounterProps) => {
+  const { t } = useTranslation()
+  const [current, setCurrent] = useState(0)
   const [total, setTotal] = useState(0)
 
   const handleIncrement = () => {
+    setCurrent(current + 1)
     setTotal(total + 1)
     incrementScore()
   }
 
   const handleDecrement = () => {
-    if (total === 0) return
-    setTotal(total - 1)
+    if (current === 0) return
+    setCurrent(current - 1)
     decrementScore()
   }
 
   return (
     <div className={style.resourceCounter}>
       <div onClick={handleDecrement} className={style.modifier}>➖</div>
-      {total}
-      <Resource type={resource} />
+      <div className={style.data}>
+        <div className={style.current}>
+          {current}
+          <Resource type={resource} />
+        </div>
+        {t('total')}: {total}
+      </div>
       <div onClick={handleIncrement} className={style.modifier}>➕</div>
     </div>
   )
